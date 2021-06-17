76bd122393979e6e3a800a59962efb0506cd794b
torstein <torstme@stud.ntnu.no>
2021-06-17 18:59:23 +0200

    fix evaluation, add noise defense

--- attacker/defender.py
+++ attacker/defender.py
@@ -1,10 +1,10 @@
 import torch
-
+from utils.torch_utils import get_device
 def defender(images, noise_levels):
     new_images = []
     for lvl in noise_levels:
         if lvl > 0:
-            noise = torch.normal(0, lvl, size=images.shape)
+            noise = torch.normal(0, lvl, size=images.shape).to(get_device())
             new_image = torch.add(images, noise)
             new_images.append(new_image)
         else:
